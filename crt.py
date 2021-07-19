b7703cb9bef8817cf8b97a2d6693ebdff0a13d6a
Nivrito <nivrita540@gmail.com>
2021-07-19 13:11:30 +0600

    Just changing the name and making the output colorful

--- crt.py
+++ crt.py
@@ -1,7 +1,13 @@
 import argparse
 import requests
 from bs4 import BeautifulSoup
-
+"""
+author: Nivrita Podder
+age: 17
+email : nivrita540@gmail.com
+date: 19 July 2021
+Disclaimer: The creator will not be responsible for any misuse of this program.
+"""
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", type=str, help= "Enter the domain name")
 parser.add_argument("-o", type=str, help="Add the file name you want to add the subdomain")
@@ -16,7 +22,7 @@ def main():
     for sub in soup.find_all('td'):
         if domain in str(sub.string) and "Type: " not in str(sub.string):
             subdomain = str(sub.string).replace("*.","")
-            print(subdomain)
+            print('\033[92m'+subdomain)
             if args.o != None:
                 try:
                     f.write(subdomain+"\n")
@@ -25,5 +31,17 @@ def main():
     if args.o != None:
         f.close()
 if __name__ == '__main__':
+    print( '\033[94m'+"""
+ ███▄    █ ██▒   █▓ ██▀███  
+ ██ ▀█   █▓██░   █▒▓██ ▒ ██▒
+▓██  ▀█ ██▒▓██  █▒░▓██ ░▄█ ▒
+▓██▒  ▐▌██▒ ▒██ █░░▒██▀▀█▄  
+▒██░   ▓██░  ▒▀█░  ░██▓ ▒██▒
+░ ▒░   ▒ ▒   ░ ▐░  ░ ▒▓ ░▒▓░
+░ ░░   ░ ▒░  ░ ░░    ░▒ ░ ▒░
+   ░   ░ ░     ░░    ░░   ░ 
+         ░      ░     ░     
+               ░            
+""")
     main()
 
